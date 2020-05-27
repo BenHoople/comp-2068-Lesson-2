bfa7cc07718a43009514862c560178ed3ee5b6b1
Ben <b.hoople@hotmail.com>
2020-05-27 15:59:27 -0400

    dev andstart addition

--- example-node.js
+++ example-node.js
@@ -1,1 +1,24 @@
 const express = require("express");
+const app = express();
+//require lib.
+const https = require('https');
+
+app.get('/',function (request, response) {
+    //controller logic
+    https.get('https://insult.mattbas.org/api/insult', resp => {
+        resp.on('data', chunk => {
+            //responding with the view
+            response.send(`
+                <http>
+                    <head><title>Oh Snap</title></head>
+                    <body>
+                        <h1>OH SNAP SON!</h1>
+                        <p>${chunk.toString()}!</p>
+                    </body>    
+                </http>
+            `)
+        })
+    });
+});
+
+app.listen(process.env.PORT || 3000);
